aafeda42f2c93e540737d16129a34eacc642770e
Rinae Tshibubudze <vharhee@gmail.com>
2025-09-16 21:58:45 +0200

    Initial Code with few code.

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import Chatboticon from "./components/chatboticon";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="chatbot-popup">
+        {/* Chat Header */}
+        <div className="chat-header">
+          <div className="header-info">
+            <Chatboticon />
+            <h2 className="logo-text">Chatbot</h2>
+          </div>
+
+          <button className="material-symbols-rounded">
+            keyboard_arrow_down
+          </button>
+        </div>
+        {/* Chat Body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+            <Chatboticon />
+            <p className="message-text">
+              Hey there! <br /> How can I assist you today?
+            </p>
+          </div>
+          <div className="message user-message">
+            <p className="message-text">How is the weather today?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
